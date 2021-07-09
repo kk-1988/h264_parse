195e129d11eace79fb429110424212c903dd66d9
xiaobo.kong <xiaobo.kong@aliyun.com>
2021-07-09 11:17:53 +0800

    完善flv parse的demo

--- simplest_flv_parser.c
+++ simplest_flv_parser.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
+int simplest_flv_parser(char *url)
+{
+   int output_a = 1;
+   int output_v = 1;
+
+   FILE*ifh = NULL,*vfg = NULL, *afg = NULL;
+
+   FILE *myout = stdout;
+
+   FLV_HEADER flv;
+
+}
+
 int main(int argc,char *argv[])
 {
     
